ea7f9e51a8d25b1701a08047eef98c41a02c4430
AvinashRapolu <avinash.rapolu@cjsstechnologies.com>
2021-12-31 11:12:32 +0530

    db

---
+++ DataBase/src/main/java/com/avinash/DataBase/DataBasePracApplication.java
@@ -0,0 +1,13 @@
+package com.avinash.DataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBasePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBasePracApplication.class, args);
+	}
+
+}
---
+++ DataBase/src/test/java/com/avinash/DataBase/DataBasePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.DataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBasePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
